24b16e0d3ca949fbda173f2c6ea723484e10cd76
Alex Kreidler <contributions@alexkreidler.com>
2021-10-04 20:59:59 -0400

    update script

--- download_transformed.sh
+++ download_transformed.sh
@@ -2,7 +2,7 @@
 # upload:
 # rclone copy /mnt/data/dbnomics/datasets --include '*.jsonld' dbnomics:dbnomics-dataset-metadata --dry-run
 
-DBNOMICS_DATA_DIR="${DBNOMICS_DATA_DIR:-"/data/dbnomics"}"
+DBNOMICS_DATA_DIR="${DBNOMICS_DATA_DIR:-"$HOME/dbnomics_data"}"
 
 mkdir $DBNOMICS_DATA_DIR
 rclone copy :gcs:static.loqudata.org $DBNOMICS_DATA_DIR
